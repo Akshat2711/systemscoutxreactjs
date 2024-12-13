41241cd00a7bf54b50d7ec1c2a14bbb525459d06
AKSHAT SRIVASTAVA <akshatsrivastava206@gmail.com>
2024-12-13 17:36:54 +0530

    removed some more api

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,15 +7,17 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAUhQnvst1RvP-JQfcoeR80ui4VstKK6Y",
-  authDomain: "testing-88bc3.firebaseapp.com",
-  databaseURL: "https://testing-88bc3-default-rtdb.firebaseio.com",
-  projectId: "testing-88bc3",
-  storageBucket: "testing-88bc3.appspot.com", // This is needed for Firebase Storage
-  messagingSenderId: "633592353172",
-  appId: "1:633592353172:web:5fa3678eacc9125a90bf81",
-  measurementId: "G-DX55Q8CSS2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
